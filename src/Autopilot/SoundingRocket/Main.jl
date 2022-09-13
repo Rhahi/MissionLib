6b6790eaaa13e62247be50ac584d7b4713414c04
Rhahi <git@rhahi.com>
2022-09-14 00:48:49 +0200

    Add sleeping rocket, a rocket that does nothing.
    This rocket was used to test software and API.

--- src/Autopilot/SoundingRocket/Main.jl
+++ src/Autopilot/SoundingRocket/Main.jl
@@ -6,28 +6,27 @@ include("./subroutines.jl")
 
 
 function setup(sp::Spacecraft)
-    @info "Create blank conditions"
+    @debug "Create blank conditions"
     sp.events["stage1"] = Condition()
     sp.events["stage2"] = Condition()
     parts = RC.Parts(sp.ves)
-    @info "Setup tagged parts"
+    @debug "Setup tagged parts"
     sp.parts["e0"] = RC.WithTag(parts, "e0")[1]
     sp.parts["e1"] = RC.WithTag(parts, "e1")[1]
     sp.parts["e2"] = RC.WithTag(parts, "e2")[1]
-    @info "Set throttle"
+    @debug "Set throttle"
     Control.throttle!(sp, 1.0)
 end
 
 
 main("SR Test"; log_path=homedir()*"/spacelib/SR", log_level=LogLevel(-50)) do sp
-    @info "Setting up..."
-    Telemetry.@telemetry "telemetry test" a = 1
+    @debug "Setting up..."
     setup(sp)
-    @info "Setup complete"
+    @debug "Setup complete"
 
-    @info "Schedule stage 0"
+    @debug "Schedule stage 0"
     s0 = @async stage0(sp)
-    @info "Schedule stage 1"
+    @debug "Schedule stage 1"
     s1 = @async stage1(sp)
 
     sleep(5)
