f030ebed104319b0699226cddbc94f448582e4f5
Rhahi <git@rhahi.com>
2022-10-09 21:28:19 +0200

    Implement parachute deployment for SR6

--- src/Autopilot/SoundingRocket/SR6.jl
+++ src/Autopilot/SoundingRocket/SR6.jl
@@ -14,12 +14,14 @@ function setup(sp::Spacecraft)
         @logprogress 1/steps info="setup triggers"
         sp.events[:stage1] = Condition()
         sp.events[:stage2] = Condition()
+        sp.events[:deploy] = Condition()
 
         @logprogress 2/steps info="setup tagged parts"
         parts = SCH.Parts(sp.ves)
         sp.parts[:e0] = SCH.WithTag(parts, "e0")[1]
         sp.parts[:e1] = SCH.WithTag(parts, "e1")[1]
         sp.parts[:e2] = SCH.WithTag(parts, "e2")[1]
+        sp.parts[:chute] = SCH.WithTag(parts, "chute")[1]
 
         @logprogress 3/steps info="set throttle"
         Control.throttle!(sp, 1.0)
@@ -34,6 +36,6 @@ main("SR Test"; log_path=homedir()*"/spacelib/SR", log_level=LogLevel(-1000)) do
     s0 = @async stage0(sp)
     s1 = @async stage1(sp)
     s2 = @async stage2(sp)
-    # dep = @async deploy(sp)
-    wait(s2)
+    dep = @async deploy(sp)
+    wait(dep)
 end
--- src/Autopilot/SoundingRocket/subroutines.jl
+++ src/Autopilot/SoundingRocket/subroutines.jl
@@ -1,6 +1,7 @@
-using SpaceLib
 using LoggingExtras
-using KRPC
+using SpaceLib
+using SpaceLib.Timing
+using SpaceLib.Control
 import KRPC.Interface.SpaceCenter.Helpers as SCH
 import KRPC.Interface.SpaceCenter as SC
 
@@ -8,60 +9,42 @@ import KRPC.Interface.SpaceCenter as SC
 function stage0(sp::Spacecraft)
     # release clamps and ignite boosters
     @info "Ignition"
-    Control.stage(sp)
-    Timing.delay(sp, 0.5)
+    stage(sp)
+    delay(sp, 0.5)
     notify(sp.events[:stage1])
-    Timing.delay(sp, 0.05)
 end
 
 
 function stage1(sp::Spacecraft)
-    @infov 1 "enter stage 1"
-    e1 = SCH.Engine(sp.parts[:e1])  # get engine part
+    @infov 1 "stage1 standby"
+    e1 = SCH.Engine(sp.parts[:e1])
     wait(sp.events[:stage1])
 
-    @infov 1 "begin stage 1"
-    SCH.Active!(e1, true)  # activate engine
-    Timing.delay(sp, 0.5)
-    Control.stage(sp)
+    SCH.Active!(e1, true)
+    delay(sp, 0.2)
+    stage(sp)
 
-    Timing.delay(sp, 38.5, "s1")
+    delay(sp, 38.5, log="stage1")
     notify(sp.events[:stage2])
 end
 
 
 function stage2(sp::Spacecraft)
-    @infov 1 "enter stage 2"
     e2 = SCH.Engine(sp.parts[:e2])
+    @infov 1 "stage2 standby"
     wait(sp.events[:stage2])
-    @infov 1 "begin stage 2"
 
     SCH.Active!(e2, true)
-    Timing.delay(sp, 0.5)
-    Control.stage(sp)
+    delay(sp, 0.5)
+    stage(sp)
+    delay(sp, 38.5, log="stage2")
+    notify(sp.events[:deploy])
 end
 
 
-# function deploy(sp::Spacecraft)
-#     delay_for_value(sp, )
-# end
-
-
-# """Delay with observable, simple strategy"""
-# function delay_for_value(sp::Spacecraft, measurement::Request, target::Number)
-#     start = kerbal(sp.conn, f())
-#     add_stream(sp.conn, (f(),)) do stream
-#         if start > target
-#             for (value,) in stream
-#                 target ≥ value && break
-#                 yield()
-#             end
-#         else
-#             for (value,) in stream
-#                 target ≤ value && break
-#                 yield()
-#             end
-#         end
-#     end
-#     return start
-# end
+function deploy(sp::Spacecraft)
+    wait(sp.events[:deploy])
+    Timing.delay_on_bedrock_altitude(sp, 140e+3, 120, "chute")
+    SCH.Arm(SCH.Parachute(sp.parts[:chute]))
+    stage(sp)
+end
