90e6f0c6a0142a93417a8d0fa7a2f4403249e2bd
Rhahi <git@rhahi.com>
2022-12-07 00:47:55 +0100

    Upload soundingrocket (deprecated)

--- src/Autopilot/SoundingRocket/SR5_SO.jl
+++ src/Autopilot/SoundingRocket/SR5_SO.jl
@@ -13,12 +13,10 @@ function setup(sp::Spacecraft)
         parts = SCH.Parts(sp.ves)
         sp.parts[:e1] = SCH.WithTag(parts, "e1")[1]
         sp.parts[:e2] = SCH.WithTag(parts, "e2")[1]
-        sp.parts[:chute] = SCH.WithTag(parts, "chute")[1]
 
         @logprogress 2/steps info="setup conditions"
         sp.events[:s1] = Condition()
         sp.events[:s2] = Condition()
-        sp.events[:deploy] = Condition()
 
         @logprogress 3/steps info="setup throttle"
         Control.throttle!(sp, 1)
@@ -30,7 +28,7 @@ end
 
 function stage0(sp::Spacecraft)
     stage!(sp)
-
+    delay(sp, 0.8)
     stage!(sp)
     delay(sp, 0.6)
     notify(sp.events[:s1])
@@ -59,28 +57,19 @@ function stage2(sp::Spacecraft)
     delay(sp, 0.3)
     stage!(sp)
     delay(sp, 40, "stage2")
-    notify(sp.events[:deploy])
     @info "notified dep"
 end
 
 
-function deploy(sp::Spacecraft)
-    chute = sp.parts[:chute]
-    wait(sp.events[:deploy])
-    delay__bedrock_altitude(sp, "radar"; target=145e+3)
-    Parachute.arm(chute)
-    stage!(sp)
-    @info "Giving a few seconds for parachute arming..."
-    delay(sp, 5)
-end
-
-
-main("Bio"; log_path=homedir()*"/spacelib/SR", log_level=LogLevel(-1000)) do sp
+main("Altitude";
+    log_path=homedir()*"/spacelib/SR",
+    log_level=LogLevel(-700),
+    save_file=false
+) do sp
     setup(sp)
     @sync begin
         @async stage0(sp)
         @async stage1(sp)
         @async stage2(sp)
-        @async deploy(sp)
     end
 end
